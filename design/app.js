3d58de6209e1a193a8b70bebf912e6712886cad8
Ian Moss <github@ilmc.co.uk>
2017-02-02 15:26:38 +0000

    add swagger ui

--- design/app.js
+++ design/app.js
@@ -1,7 +1,12 @@
 'use strict';
 
 var SwaggerExpress = require('swagger-express-mw');
-var app = require('express')();
+// var app = require('express')();
+
+var express = require('express');
+var app = express();
+var SwaggerUi = require('swagger-tools/middleware/swagger-ui');
+
 module.exports = app; // for testing
 
 var config = {
@@ -11,11 +16,20 @@ var config = {
 SwaggerExpress.create(config, function(err, swaggerExpress) {
   if (err) { throw err; }
 
+  // Use Swagger UI in this app
+  app.use(SwaggerUi(swaggerExpress.runner.swagger));
+  // app.use('/static', express.static('static'));
+  // app.use('/school', express.static('app/dist'));
+
   // install middleware
   swaggerExpress.register(app);
 
-  var port = process.env.PORT || 10010;
-  app.listen(port);
+  var port = process.env.PORT || 5000;
+  // app.listen(port);
+
+  app.listen(port, '0.0.0.0');
+
+
 
   if (swaggerExpress.runner.swagger.paths['/hello']) {
     console.log('try this:\ncurl http://127.0.0.1:' + port + '/hello?name=Scott');
